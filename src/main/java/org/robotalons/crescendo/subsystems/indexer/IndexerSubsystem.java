28dabe56464595fe710f91fb3784db7bbae844f5
Cody Washington <115746975+Jelatinone@users.noreply.github.com>
2024-01-13 03:13:44 -0600

    Fix Formatting, File Location, and Naming

---
+++ src/main/java/org/robotalons/crescendo/subsystems/indexer/IndexerSubsystem.java
@@ -0,0 +1,48 @@
+// ----------------------------------------------------------------[Package]----------------------------------------------------------------//
+package org.robotalons.crescendo.subsystems.indexer;
+// ---------------------------------------------------------------[Libraries]---------------------------------------------------------------//
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+// ------------------------------------------------------------[Indexer Subsystem]----------------------------------------------------------//
+/**
+ *
+ *
+ * <h1>IndexerSubsystem</h1>
+ *
+ * <p>Utility class which controls the indexing of notes from and to the intake and shooter.<p>
+ * 
+ * @see SubsystemBase
+ * @see {@link org.robotalons.crescendo.RobotContainer RobotContainer} 
+ */
+public class IndexerSubsystem extends SubsystemBase {
+  // --------------------------------------------------------------[Constants]--------------------------------------------------------------//
+  //private static final InfraredEmitter EmitterLeft;    //<---- This class does not exist, is it a placeholder? @SpiderFace
+  //private static final InfraredEmitter EmitterRight; 
+  //private static final InfraredReceiver ReceiverLeft;  //<---- This class does not exist, is it a placeholder? @SpiderFace
+  //private static final InfraredReceiver ReceiverRight;
+  // ---------------------------------------------------------------[Fields]----------------------------------------------------------------//
+  private static Boolean ContainsNote;
+  // ------------------------------------------------------------[Constructors]-------------------------------------------------------------//
+  private IndexerSubsystem() {} static {
+    ContainsNote = (false);
+  }
+  
+  // ---------------------------------------------------------------[Methods]---------------------------------------------------------------//
+  @Override
+  public synchronized void periodic()
+  {
+    //ContainsNote = !(ReceiverLeft.status() || ReceiverRight.status()); //status() should return true for an unbroken beam
+  }
+  // --------------------------------------------------------------[Internal]---------------------------------------------------------------//
+
+  // --------------------------------------------------------------[Mutators]---------------------------------------------------------------//
+  
+  // --------------------------------------------------------------[Accessors]--------------------------------------------------------------//
+  /**
+   * Provides a boolean representation of if this indexer is currently holding a not or not.
+   * @return Boolean of if a note is being held
+   */
+  public Boolean getHoldingNote() {
+    return ContainsNote;
+  }
+}
