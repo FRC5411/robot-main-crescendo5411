00c278ac720d179f47df504a7e4256a0bb23b3f2
Cody Washington <115746975+Jelatinone@users.noreply.github.com>
2024-01-16 19:15:47 -0600

    Update Constants With Correct IDs

--- src/main/java/org/robotalons/crescendo/subsystems/drivebase/Constants.java
+++ src/main/java/org/robotalons/crescendo/subsystems/drivebase/Constants.java
@@ -1,7 +1,6 @@
 // ----------------------------------------------------------------[Package]----------------------------------------------------------------//
 package org.robotalons.crescendo.subsystems.drivebase;
 // ---------------------------------------------------------------[Libraries]---------------------------------------------------------------//
-
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.util.Units;
@@ -29,154 +28,157 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public final class Constants {
   // ------------------------------------------------------------[Internal]-------------------------------------------------------------//
-    public static final class Measurements {
-        public static final Double ROBOT_WHEEL_DIAMETER_METERS = Units.inchesToMeters((4));
-        public static final Double ROBOT_WHEEL_PERIMETER_METERS = ROBOT_WHEEL_DIAMETER_METERS * Math.PI;
-        public static final Double ROBOT_LENGTH_METERS = Units.inchesToMeters((29));        
-        public static final Double ROBOT_WIDTH_METERS = Units.inchesToMeters((29));
-        public static final Double ROBOT_RADIUS_METERS = Math.hypot(ROBOT_LENGTH_METERS / (2.0), ROBOT_WIDTH_METERS / (2.0));      
+public static final class Measurements {
+    public static final Double ROBOT_WHEEL_DIAMETER_METERS = Units.inchesToMeters((4));
+    public static final Double ROBOT_WHEEL_PERIMETER_METERS = ROBOT_WHEEL_DIAMETER_METERS * Math.PI;
+    public static final Double ROBOT_LENGTH_METERS = Units.inchesToMeters((29));        
+    public static final Double ROBOT_WIDTH_METERS = Units.inchesToMeters((29));
+    public static final Double ROBOT_RADIUS_METERS = Math.hypot(ROBOT_LENGTH_METERS / (2.0), ROBOT_WIDTH_METERS / (2.0));      
 
-        public static final Double ROBOT_MAXIMUM_LINEAR_VELOCITY = Units.feetToMeters((15.4));
-        public static final Double ROBOT_MAXIMUM_ANGULAR_VELOCITY = ROBOT_MAXIMUM_LINEAR_VELOCITY / ROBOT_RADIUS_METERS;
+    public static final Double ROBOT_MAXIMUM_LINEAR_VELOCITY = Units.feetToMeters((15.4));
+    public static final Double ROBOT_MAXIMUM_ANGULAR_VELOCITY = ROBOT_MAXIMUM_LINEAR_VELOCITY / ROBOT_RADIUS_METERS;
 
-        public static final Double ROBOT_TRANSLATION_KP = (0d);
-        public static final Double ROBOT_TRANSLATION_KI = (0d);
-        public static final Double ROBOT_TRANSLATION_KD = (0d);    
+    public static final Double ROBOT_TRANSLATION_KP = (1d);
+    public static final Double ROBOT_TRANSLATION_KI = (1d);
+    public static final Double ROBOT_TRANSLATION_KD = (1d);    
 
-        public static final Double ROBOT_ROTATIONAL_KP = (0d);
-        public static final Double ROBOT_ROTATIONAL_KI = (0d);
-        public static final Double ROBOT_ROTATIONAL_KD = (0d);
+    public static final Double ROBOT_ROTATIONAL_KP = (1d);
+    public static final Double ROBOT_ROTATIONAL_KI = (1d);
+    public static final Double ROBOT_ROTATIONAL_KD = (1d);
 
-        public static final Boolean PHOENIX_DRIVE = (false);
+    public static final Boolean PHOENIX_DRIVE = (false);
 
-        public static final Double ODOMETRY_FREQUENCY = (250d);
+    public static final Double ODOMETRY_FREQUENCY = (250d);
 
-        public static final class Modules {
-          public static final class FL {
-            public static final Integer LINEAR_CONTROLLER_ID = (0);
-            public static final Integer ROTATIONAL_CONTROLLER_ID = (1);
-            public static final Integer ABSOLUTE_ENCODER_ID = (8);
-            public static final Double ROTATIONAL_P_GAIN = (0d);
-            public static final Double ROTATIONAL_I_GAIN = (0d);
-            public static final Double ROTATIONAL_D_GAIN = (0d);
-            public static final Double LINEAR_P_GAIN = (0d);
-            public static final Double LINEAR_I_GAIN = (0d);
-            public static final Double LINEAR_D_GAIN = (0d);
-            public static final Double LINEAR_KS_GAIN = (0d);
-            public static final Double LINEAR_KV_GAIN = (0d);
-            public static final Double LINEAR_KA_GAIN = (0d);
-            public static final ModuleConstants CONSTANTS = new ModuleConstants();
-            static {
-              CONSTANTS.LINEAR_CONTROLLER = new CANSparkMax(LINEAR_CONTROLLER_ID, MotorType.kBrushless);
-              CONSTANTS.ROTATIONAL_CONTROLLER = new CANSparkMax(ROTATIONAL_CONTROLLER_ID, MotorType.kBrushless);
-              CONSTANTS.ABSOLUTE_ENCODER = new WPI_CANCoder(ABSOLUTE_ENCODER_ID);
-              CONSTANTS.LINEAR_CONTROLLER_PID = new PIDController(LINEAR_P_GAIN, LINEAR_I_GAIN, LINEAR_D_GAIN);
-              CONSTANTS.ROTATIONAL_CONTROLLER_PID = new PIDController(ROTATIONAL_P_GAIN, ROTATIONAL_I_GAIN, ROTATIONAL_D_GAIN);
-              CONSTANTS.LINEAR_CONTROLLER_FEEDFORWARD = new SimpleMotorFeedforward(LINEAR_KS_GAIN, LINEAR_KV_GAIN, LINEAR_KA_GAIN);
-              CONSTANTS.WHEEL_RADIUS_METERS = Measurements.ROBOT_WHEEL_DIAMETER_METERS / 2;
-              CONSTANTS.NUMBER = 0;
-            }
-          }
-  
-          public static final class FR {
-            public static final Integer LINEAR_CONTROLLER_ID = (2);
-            public static final Integer ROTATIONAL_CONTROLLER_ID = (3);
-            public static final Integer ABSOLUTE_ENCODER_ID = (9);
-            public static final Double ROTATIONAL_P_GAIN = (0d);
-            public static final Double ROTATIONAL_I_GAIN = (0d);
-            public static final Double ROTATIONAL_D_GAIN = (0d);
-            public static final Double LINEAR_P_GAIN = (0d);
-            public static final Double LINEAR_I_GAIN = (0d);
-            public static final Double LINEAR_D_GAIN = (0d);
-            public static final Double LINEAR_KS_GAIN = (0d);
-            public static final Double LINEAR_KV_GAIN = (0d);
-            public static final Double LINEAR_KA_GAIN = (0d);
-            public static final ModuleConstants CONSTANTS = new ModuleConstants();
-            static {
-              CONSTANTS.LINEAR_CONTROLLER = new CANSparkMax(LINEAR_CONTROLLER_ID, MotorType.kBrushless);
-              CONSTANTS.ROTATIONAL_CONTROLLER = new CANSparkMax(ROTATIONAL_CONTROLLER_ID, MotorType.kBrushless);
-              CONSTANTS.ABSOLUTE_ENCODER = new WPI_CANCoder(ABSOLUTE_ENCODER_ID);
-              CONSTANTS.LINEAR_CONTROLLER_PID = new PIDController(LINEAR_P_GAIN, LINEAR_I_GAIN, LINEAR_D_GAIN);
-              CONSTANTS.ROTATIONAL_CONTROLLER_PID = new PIDController(ROTATIONAL_P_GAIN, ROTATIONAL_I_GAIN, ROTATIONAL_D_GAIN);
-              CONSTANTS.LINEAR_CONTROLLER_FEEDFORWARD = new SimpleMotorFeedforward(LINEAR_KS_GAIN, LINEAR_KV_GAIN, LINEAR_KA_GAIN);
-              CONSTANTS.WHEEL_RADIUS_METERS = Measurements.ROBOT_WHEEL_DIAMETER_METERS / 2;
-              CONSTANTS.NUMBER = 1;
-            }
-          }
-  
-          public static final class RL {
-            public static final Integer LINEAR_CONTROLLER_ID = (4);
-            public static final Integer ROTATIONAL_CONTROLLER_ID = (5);
-            public static final Integer ABSOLUTE_ENCODER_ID = (10);
-            public static final Double ROTATIONAL_P_GAIN = (0d);
-            public static final Double ROTATIONAL_I_GAIN = (0d);
-            public static final Double ROTATIONAL_D_GAIN = (0d);
-            public static final Double LINEAR_P_GAIN = (0d);
-            public static final Double LINEAR_I_GAIN = (0d);
-            public static final Double LINEAR_D_GAIN = (0d);
-            public static final Double LINEAR_KS_GAIN = (0d);
-            public static final Double LINEAR_KV_GAIN = (0d);
-            public static final Double LINEAR_KA_GAIN = (0d);
-            public static final ModuleConstants CONSTANTS = new ModuleConstants();
-            static {
-              CONSTANTS.LINEAR_CONTROLLER = new CANSparkMax(LINEAR_CONTROLLER_ID, MotorType.kBrushless);
-              CONSTANTS.ROTATIONAL_CONTROLLER = new CANSparkMax(ROTATIONAL_CONTROLLER_ID, MotorType.kBrushless);
-              CONSTANTS.ABSOLUTE_ENCODER = new WPI_CANCoder(ABSOLUTE_ENCODER_ID);
-              CONSTANTS.LINEAR_CONTROLLER_PID = new PIDController(LINEAR_P_GAIN, LINEAR_I_GAIN, LINEAR_D_GAIN);
-              CONSTANTS.ROTATIONAL_CONTROLLER_PID = new PIDController(ROTATIONAL_P_GAIN, ROTATIONAL_I_GAIN, ROTATIONAL_D_GAIN);
-              CONSTANTS.LINEAR_CONTROLLER_FEEDFORWARD = new SimpleMotorFeedforward(LINEAR_KS_GAIN, LINEAR_KV_GAIN, LINEAR_KA_GAIN);
-              CONSTANTS.WHEEL_RADIUS_METERS = Measurements.ROBOT_WHEEL_DIAMETER_METERS / 2;
-              CONSTANTS.NUMBER = 2;
-            }
-          }
-  
-          public static final class RR {
-            public static final Integer LINEAR_CONTROLLER_ID = (6);
-            public static final Integer ROTATIONAL_CONTROLLER_ID = (7);
-            public static final Integer ABSOLUTE_ENCODER_ID = (11);
-            public static final Double ROTATIONAL_P_GAIN = (0d);
-            public static final Double ROTATIONAL_I_GAIN = (0d);
-            public static final Double ROTATIONAL_D_GAIN = (0d);
-            public static final Double LINEAR_P_GAIN = (0d);
-            public static final Double LINEAR_I_GAIN = (0d);
-            public static final Double LINEAR_D_GAIN = (0d);
-            public static final Double LINEAR_KS_GAIN = (0d);
-            public static final Double LINEAR_KV_GAIN = (0d);
-            public static final Double LINEAR_KA_GAIN = (0d);
-            public static final ModuleConstants CONSTANTS = new ModuleConstants();
-            static {
-              CONSTANTS.LINEAR_CONTROLLER = new CANSparkMax(LINEAR_CONTROLLER_ID, MotorType.kBrushless);
-              CONSTANTS.ROTATIONAL_CONTROLLER = new CANSparkMax(ROTATIONAL_CONTROLLER_ID, MotorType.kBrushless);
-              CONSTANTS.ABSOLUTE_ENCODER = new WPI_CANCoder(ABSOLUTE_ENCODER_ID);
-              CONSTANTS.LINEAR_CONTROLLER_PID = new PIDController(LINEAR_P_GAIN, LINEAR_I_GAIN, LINEAR_D_GAIN);
-              CONSTANTS.ROTATIONAL_CONTROLLER_PID = new PIDController(ROTATIONAL_P_GAIN, ROTATIONAL_I_GAIN, ROTATIONAL_D_GAIN);
-              CONSTANTS.LINEAR_CONTROLLER_FEEDFORWARD = new SimpleMotorFeedforward(LINEAR_KS_GAIN, LINEAR_KV_GAIN, LINEAR_KA_GAIN);
-              CONSTANTS.WHEEL_RADIUS_METERS = Measurements.ROBOT_WHEEL_DIAMETER_METERS / 2;
-              CONSTANTS.NUMBER = 3;
-            }
-          }
-  
+    public static final class Modules {
+      public static final class FL {
+        public static final Integer LINEAR_CONTROLLER_ID = (11);
+        public static final Integer ROTATIONAL_CONTROLLER_ID = (21);
+        public static final Integer ABSOLUTE_ENCODER_ID = (31);
+        public static final Double ROTATIONAL_P_GAIN = (1d);
+        public static final Double ROTATIONAL_I_GAIN = (1d);
+        public static final Double ROTATIONAL_D_GAIN = (1d);
+        public static final Double LINEAR_P_GAIN = (1d);
+        public static final Double LINEAR_I_GAIN = (1d);
+        public static final Double LINEAR_D_GAIN = (1d);
+        public static final Double LINEAR_KS_GAIN = (1d);
+        public static final Double LINEAR_KV_GAIN = (1d);
+        public static final Double LINEAR_KA_GAIN = (1d);
+        public static final Integer NUMBER = 0;
+        public static final ModuleConstants CONSTANTS = new ModuleConstants();
+        static {
+          CONSTANTS.LINEAR_CONTROLLER = new CANSparkMax(LINEAR_CONTROLLER_ID, MotorType.kBrushless);
+          CONSTANTS.ROTATIONAL_CONTROLLER = new CANSparkMax(ROTATIONAL_CONTROLLER_ID, MotorType.kBrushless);
+          CONSTANTS.ABSOLUTE_ENCODER = new WPI_CANCoder(ABSOLUTE_ENCODER_ID);
+          CONSTANTS.LINEAR_CONTROLLER_PID = new PIDController(LINEAR_P_GAIN, LINEAR_I_GAIN, LINEAR_D_GAIN);
+          CONSTANTS.ROTATIONAL_CONTROLLER_PID = new PIDController(ROTATIONAL_P_GAIN, ROTATIONAL_I_GAIN, ROTATIONAL_D_GAIN);
+          CONSTANTS.LINEAR_CONTROLLER_FEEDFORWARD = new SimpleMotorFeedforward(LINEAR_KS_GAIN, LINEAR_KV_GAIN, LINEAR_KA_GAIN);
+          CONSTANTS.WHEEL_RADIUS_METERS = Measurements.ROBOT_WHEEL_DIAMETER_METERS / 2;
+          CONSTANTS.NUMBER = NUMBER;
         }
-    }
+      }
 
-    public static final class Ports {
-      public static final Integer GYROSCOPE_ID = (0);      
-    }
+      public static final class FR {
+        public static final Integer LINEAR_CONTROLLER_ID = (12);
+        public static final Integer ROTATIONAL_CONTROLLER_ID = (22);
+        public static final Integer ABSOLUTE_ENCODER_ID = (32);
+        public static final Double ROTATIONAL_P_GAIN = (1d);
+        public static final Double ROTATIONAL_I_GAIN = (1d);
+        public static final Double ROTATIONAL_D_GAIN = (1d);
+        public static final Double LINEAR_P_GAIN = (1d);
+        public static final Double LINEAR_I_GAIN = (1d);
+        public static final Double LINEAR_D_GAIN = (1d);
+        public static final Double LINEAR_KS_GAIN = (1d);
+        public static final Double LINEAR_KV_GAIN = (1d);
+        public static final Double LINEAR_KA_GAIN = (1d);
+        public static final Integer NUMBER = 0;
+        public static final ModuleConstants CONSTANTS = new ModuleConstants();
+        static {
+          CONSTANTS.LINEAR_CONTROLLER = new CANSparkMax(LINEAR_CONTROLLER_ID, MotorType.kBrushless);
+          CONSTANTS.ROTATIONAL_CONTROLLER = new CANSparkMax(ROTATIONAL_CONTROLLER_ID, MotorType.kBrushless);
+          CONSTANTS.ABSOLUTE_ENCODER = new WPI_CANCoder(ABSOLUTE_ENCODER_ID);
+          CONSTANTS.LINEAR_CONTROLLER_PID = new PIDController(LINEAR_P_GAIN, LINEAR_I_GAIN, LINEAR_D_GAIN);
+          CONSTANTS.ROTATIONAL_CONTROLLER_PID = new PIDController(ROTATIONAL_P_GAIN, ROTATIONAL_I_GAIN, ROTATIONAL_D_GAIN);
+          CONSTANTS.LINEAR_CONTROLLER_FEEDFORWARD = new SimpleMotorFeedforward(LINEAR_KS_GAIN, LINEAR_KV_GAIN, LINEAR_KA_GAIN);
+          CONSTANTS.WHEEL_RADIUS_METERS = Measurements.ROBOT_WHEEL_DIAMETER_METERS / 2;
+          CONSTANTS.NUMBER = NUMBER;
+        }
+      }
 
-    public static final class Objects {
-        public static final Lock ODOMETRY_LOCK = new ReentrantLock();
-    }
+      public static final class RL {
+        public static final Integer LINEAR_CONTROLLER_ID = (13);
+        public static final Integer ROTATIONAL_CONTROLLER_ID = (23);
+        public static final Integer ABSOLUTE_ENCODER_ID = (33);
+        public static final Double ROTATIONAL_P_GAIN = (1d);
+        public static final Double ROTATIONAL_I_GAIN = (1d);
+        public static final Double ROTATIONAL_D_GAIN = (1d);
+        public static final Double LINEAR_P_GAIN = (1d);
+        public static final Double LINEAR_I_GAIN = (1d);
+        public static final Double LINEAR_D_GAIN = (1d);
+        public static final Double LINEAR_KS_GAIN = (1d);
+        public static final Double LINEAR_KV_GAIN = (1d);
+        public static final Double LINEAR_KA_GAIN = (1d);
+        public static final Integer NUMBER = 0;
+        public static final ModuleConstants CONSTANTS = new ModuleConstants();
+        static {
+          CONSTANTS.LINEAR_CONTROLLER = new CANSparkMax(LINEAR_CONTROLLER_ID, MotorType.kBrushless);
+          CONSTANTS.ROTATIONAL_CONTROLLER = new CANSparkMax(ROTATIONAL_CONTROLLER_ID, MotorType.kBrushless);
+          CONSTANTS.ABSOLUTE_ENCODER = new WPI_CANCoder(ABSOLUTE_ENCODER_ID);
+          CONSTANTS.LINEAR_CONTROLLER_PID = new PIDController(LINEAR_P_GAIN, LINEAR_I_GAIN, LINEAR_D_GAIN);
+          CONSTANTS.ROTATIONAL_CONTROLLER_PID = new PIDController(ROTATIONAL_P_GAIN, ROTATIONAL_I_GAIN, ROTATIONAL_D_GAIN);
+          CONSTANTS.LINEAR_CONTROLLER_FEEDFORWARD = new SimpleMotorFeedforward(LINEAR_KS_GAIN, LINEAR_KV_GAIN, LINEAR_KA_GAIN);
+          CONSTANTS.WHEEL_RADIUS_METERS = Measurements.ROBOT_WHEEL_DIAMETER_METERS / 2;
+          CONSTANTS.NUMBER = NUMBER;
+        }
+      }
 
-    public static final class Devices {
-      public static final Gyroscope GYROSCOPE = 
-        new PigeonGyroscope(Measurements.PHOENIX_DRIVE);
-      public static final Module FRONT_LEFT_MODULE = 
-        new REVControllerModule(Measurements.Modules.FL.CONSTANTS);     
-      public static final Module FRONT_RIGHT_MODULE = 
-        new REVControllerModule(Measurements.Modules.FR.CONSTANTS);             
-      public static final Module REAR_LEFT_MODULE = 
-        new REVControllerModule(Measurements.Modules.RL.CONSTANTS);        
-      public static final Module REAR_RIGHT_MODULE = 
-        new REVControllerModule(Measurements.Modules.RR.CONSTANTS);    
+      public static final class RR {
+        public static final Integer LINEAR_CONTROLLER_ID = (14);
+        public static final Integer ROTATIONAL_CONTROLLER_ID = (24);
+        public static final Integer ABSOLUTE_ENCODER_ID = (34);
+        public static final Double ROTATIONAL_P_GAIN = (1d);
+        public static final Double ROTATIONAL_I_GAIN = (1d);
+        public static final Double ROTATIONAL_D_GAIN = (1d);
+        public static final Double LINEAR_P_GAIN = (1d);
+        public static final Double LINEAR_I_GAIN = (1d);
+        public static final Double LINEAR_D_GAIN = (1d);
+        public static final Double LINEAR_KS_GAIN = (1d);
+        public static final Double LINEAR_KV_GAIN = (1d);
+        public static final Double LINEAR_KA_GAIN = (1d);
+        public static final Integer NUMBER = 0;
+        public static final ModuleConstants CONSTANTS = new ModuleConstants();
+        static {
+          CONSTANTS.LINEAR_CONTROLLER = new CANSparkMax(LINEAR_CONTROLLER_ID, MotorType.kBrushless);
+          CONSTANTS.ROTATIONAL_CONTROLLER = new CANSparkMax(ROTATIONAL_CONTROLLER_ID, MotorType.kBrushless);
+          CONSTANTS.ABSOLUTE_ENCODER = new WPI_CANCoder(ABSOLUTE_ENCODER_ID);
+          CONSTANTS.LINEAR_CONTROLLER_PID = new PIDController(LINEAR_P_GAIN, LINEAR_I_GAIN, LINEAR_D_GAIN);
+          CONSTANTS.ROTATIONAL_CONTROLLER_PID = new PIDController(ROTATIONAL_P_GAIN, ROTATIONAL_I_GAIN, ROTATIONAL_D_GAIN);
+          CONSTANTS.LINEAR_CONTROLLER_FEEDFORWARD = new SimpleMotorFeedforward(LINEAR_KS_GAIN, LINEAR_KV_GAIN, LINEAR_KA_GAIN);
+          CONSTANTS.WHEEL_RADIUS_METERS = Measurements.ROBOT_WHEEL_DIAMETER_METERS / 2;
+          CONSTANTS.NUMBER = NUMBER;
+        }
+      }
+    }
+  }
+
+  public static final class Ports {
+    public static final Integer GYROSCOPE_ID = (10);      
+  }
+
+  public static final class Objects {
+      public static final Lock ODOMETRY_LOCK = new ReentrantLock();
+  }
+
+  public static final class Devices {
+    public static final Gyroscope GYROSCOPE = 
+      new PigeonGyroscope(Measurements.PHOENIX_DRIVE);
+    public static final Module FRONT_LEFT_MODULE = 
+      new REVControllerModule(Measurements.Modules.FL.CONSTANTS);     
+    public static final Module FRONT_RIGHT_MODULE = 
+      new REVControllerModule(Measurements.Modules.FR.CONSTANTS);             
+    public static final Module REAR_LEFT_MODULE = 
+      new REVControllerModule(Measurements.Modules.RL.CONSTANTS);        
+    public static final Module REAR_RIGHT_MODULE = 
+      new REVControllerModule(Measurements.Modules.RR.CONSTANTS);    
     }
   }
