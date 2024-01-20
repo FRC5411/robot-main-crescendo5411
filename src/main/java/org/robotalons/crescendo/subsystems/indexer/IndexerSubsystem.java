99ddfb5b4f054378817eb972f157a00f40f8c837
Cody Washington <115746975+Jelatinone@users.noreply.github.com>
2024-01-20 15:21:23 -0600

    Build Test Push

--- src/main/java/org/robotalons/crescendo/subsystems/indexer/IndexerSubsystem.java
+++ src/main/java/org/robotalons/crescendo/subsystems/indexer/IndexerSubsystem.java
@@ -1,8 +1,8 @@
 // ----------------------------------------------------------------[Package]---------------------------------------------------------------- //
 package org.robotalons.crescendo.subsystems.indexer;
 // ---------------------------------------------------------------[Libraries]--------------------------------------------------------------- //
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
