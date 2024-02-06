3d597dbb3b9daeb6d49c7cc9cfbd5a0a393ae3b1
AryanKammili <140930533+AryanKammili@users.noreply.github.com>
2024-02-06 01:33:33 -0600

    Added Motor configs, accessors, and telementry

--- src/main/java/org/robotalons/crescendo/subsystems/climb/ClimbSubsystem.java
+++ src/main/java/org/robotalons/crescendo/subsystems/climb/ClimbSubsystem.java
@@ -6,6 +6,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.ControlType;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMax.SoftLimitDirection;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkMaxPIDController;
@@ -27,14 +29,16 @@ import org.littletonrobotics.junction.Logger;
  */
 public class ClimbSubsystem extends SubsystemBase {
   // --------------------------------------------------------------[Constants]-------------------------------------------------------------- //
-  private static final CANSparkMax LEFT_CONTROLLER;  
-  private static final CANSparkMax RIGHT_CONTROLLER; 
+  private static final CANSparkMax LEFT_ARM;  
+  private static final CANSparkMax RIGHT_ARM; 
   private static final RelativeEncoder LEFT_ENCODER;
   private static final RelativeEncoder RIGHT_ENCODER;
   private static final SparkMaxPIDController LEFT_PID;
   private static final SparkMaxPIDController RIGHT_PID;
   private static final ArmFeedforward LEFT_FEEDFORWARD;
   private static final ArmFeedforward RIGHT_FEEDFORWARD;
+  private static final CANSparkMax[] MOTORS;
+  private static final RelativeEncoder[] ENCODERS;
   // ---------------------------------------------------------------[Fields]---------------------------------------------------------------- //
   private static ClimbSubsystem Instance;
   // ------------------------------------------------------------[Constructors]------------------------------------------------------------- //
@@ -42,16 +46,23 @@ public class ClimbSubsystem extends SubsystemBase {
    * Indexer Subsystem Constructor
    */
   public ClimbSubsystem() {} static {
-    LEFT_CONTROLLER = new CANSparkMax(Constants.Ports.LEFT_ARM_CONTROLLER_ID, MotorType.kBrushless);
-    RIGHT_CONTROLLER = new CANSparkMax(Constants.Ports.RIGHT_ARM_CONTROLLER_ID, MotorType.kBrushless);
-    LEFT_ENCODER = LEFT_CONTROLLER.getEncoder();
-    //TODO: Add Left Encoder Tick To Radian Configurations
-    RIGHT_ENCODER = RIGHT_CONTROLLER.getEncoder();
-    //TODO: Add Right Encoder Tick To Radian Configurations
-    LEFT_PID = LEFT_CONTROLLER.getPIDController();
-    //TODO: Add Left PID Configurations
-    RIGHT_PID = RIGHT_CONTROLLER.getPIDController();
-    //TODO: Add Right PID Configurations
+    LEFT_ARM = new CANSparkMax(Constants.Ports.LEFT_ARM_CONTROLLER_ID, MotorType.kBrushless);
+    RIGHT_ARM = new CANSparkMax(Constants.Ports.RIGHT_ARM_CONTROLLER_ID, MotorType.kBrushless);
+
+    LEFT_ENCODER = LEFT_ARM.getEncoder();
+    LEFT_ENCODER.setPositionConversionFactor(org.robotalons.crescendo.subsystems.climb.Constants.Measurements.K_TICKS_2_RAD);
+
+    RIGHT_ENCODER = RIGHT_ARM.getEncoder();
+    RIGHT_ENCODER.setPositionConversionFactor(org.robotalons.crescendo.subsystems.climb.Constants.Measurements.K_TICKS_2_RAD);
+
+
+    LEFT_PID = LEFT_ARM.getPIDController();
+    RIGHT_PID = RIGHT_ARM.getPIDController();
+
+    configPID();
+
+    config(RIGHT_ARM);
+    config(LEFT_ARM);
 
     LEFT_FEEDFORWARD = new ArmFeedforward(
       Constants.Measurements.LEFT_ARM_KS,
@@ -66,6 +77,10 @@ public class ClimbSubsystem extends SubsystemBase {
       Constants.Measurements.RIGHT_ARM_KV,
       Constants.Measurements.RIGHT_ARM_KA
     );
+
+    MOTORS = new CANSparkMax[]{LEFT_ARM, RIGHT_ARM};
+    ENCODERS = new RelativeEncoder[]{LEFT_ENCODER, RIGHT_ENCODER};
+
   }
   
   // ---------------------------------------------------------------[Methods]--------------------------------------------------------------- //
@@ -82,18 +97,50 @@ public class ClimbSubsystem extends SubsystemBase {
    * Closes this instance and all held resources immediately 
    */
   public synchronized void close() {
-    LEFT_CONTROLLER.close();
-    RIGHT_CONTROLLER.close();
+    LEFT_ARM.close();
+    RIGHT_ARM.close();
+  }
+
+  // TODO: Check
+  public synchronized static void configPID(){
+
+    LEFT_PID.setFeedbackDevice(LEFT_ENCODER);
+    LEFT_PID.setP(org.robotalons.crescendo.subsystems.climb.Constants.Measurements.LEFT_ARM_P);
+    LEFT_PID.setI(org.robotalons.crescendo.subsystems.climb.Constants.Measurements.LEFT_ARM_I);
+    LEFT_PID.setD(org.robotalons.crescendo.subsystems.climb.Constants.Measurements.LEFT_ARM_D);
+
+    RIGHT_PID.setFeedbackDevice(RIGHT_ENCODER);
+    RIGHT_PID.setP(org.robotalons.crescendo.subsystems.climb.Constants.Measurements.RIGHT_ARM_P);
+    RIGHT_PID.setI(org.robotalons.crescendo.subsystems.climb.Constants.Measurements.RIGHT_ARM_I);
+    RIGHT_PID.setD(org.robotalons.crescendo.subsystems.climb.Constants.Measurements.RIGHT_ARM_D);
+  }
+
+  //TODO: Run by Isaac
+  public synchronized static void config(CANSparkMax motor){
+    motor.setIdleMode(IdleMode.kCoast);
+    motor.restoreFactoryDefaults();
+    motor.clearFaults();
+    
+    motor.setSmartCurrentLimit(org.robotalons.crescendo.subsystems.climb.Constants.Measurements.K_CURRENT_LIMIT);
+
+    motor.setSoftLimit(
+      SoftLimitDirection.kForward, 
+      org.robotalons.crescendo.subsystems.climb.Constants.Measurements.K_FORWARD_ARM_LIMIT);
+    
+    motor.setSoftLimit(
+      SoftLimitDirection.kReverse, 
+      org.robotalons.crescendo.subsystems.climb.Constants.Measurements.K_REVERSE_ARM_LIMIT);
   }
   // --------------------------------------------------------------[Internal]--------------------------------------------------------------- //
 
   // --------------------------------------------------------------[Mutators]--------------------------------------------------------------- //
+  
   /**
    * Mutates the current reference for both arms with a new demand, goal, or 'set-point'
    * @param LeftDemand Desired rotation of the left arm in radians
    * @param RightDemand Desired rotation of the right arm in radians
    */
-  public synchronized void set(final Double LeftDemand, final Double RightDemand) {
+  public synchronized void pidSet(final Double LeftDemand, final Double RightDemand) {
     //TODO: PID Implementation Here
     LEFT_PID.setReference(
       LeftDemand, 
@@ -101,7 +148,8 @@ public class ClimbSubsystem extends SubsystemBase {
       (0), 
       LEFT_FEEDFORWARD.calculate(0, 0), 
       SparkMaxPIDController.ArbFFUnits.kVoltage);
-    RIGHT_PID.setReference(
+    
+      RIGHT_PID.setReference(
       RightDemand, 
       ControlType.kPosition, 
       (0), 
@@ -111,14 +159,54 @@ public class ClimbSubsystem extends SubsystemBase {
 
   /**
    * Mutates the current reference with a new demand, goal, or 'set-point'
-   * @param Demand Desired rotation of both arms in radians
+   * @param DEMAND Desired rotation of both arms in radians
    */
-  public synchronized void set(final Double Demand) {
-    set(Demand,Demand);
+  public synchronized void pidSet(final Double DEMAND) {
+    pidSet(DEMAND,DEMAND);
+  }
+
+  /**
+   * Sets the selected motor to a certain demand from 0 - 1 on the motors
+   * 0 - Left Arm, 1 - Right Arm
+   * @param NUM Desired Motor that wants to be called
+   * @param DEMAND Desired demand on the motor
+   */
+  public synchronized void set(final Integer NUM, final Double DEMAND){
+    CANSparkMax MOTOR = MOTORS[NUM];
+    MOTOR.set(DEMAND);
+  }
+
+  /**
+   * Sets the motor to a certain demand from 0 - 1 on the motors
+   * @param DEMAND Desired demand on both motors
+   */
+  public synchronized void set(final Double DEMAND){
+    RIGHT_ARM.set(DEMAND);
+    LEFT_ARM.set(DEMAND);
   }
   // --------------------------------------------------------------[Accessors]-------------------------------------------------------------- //
   
-  //TODO: Add Arm Angle & Velocity Acessors (PROPERLY JAVADOC COMMENTED)
+  /**
+   * Returns the motors' velocity for the specified motors
+   * 0 - Left Arm, 1 - Right Arm
+   * @param NUM Picks Motors
+   * @return Velocity of Motor
+   */
+  public static synchronized Double getVelocity(Integer NUM){
+    RelativeEncoder ENCODER = ENCODERS[NUM];
+    return ENCODER.getVelocity();
+  }
+
+   /**
+   * Returns the motors' posistion for the specified motors
+   * 0 - Left Arm, 1 - Right Arm
+   * @param NUM Picks Motors
+   * @return Posistion of Motor
+   */
+  public static synchronized Double getPosistion(Integer NUM){
+    RelativeEncoder ENCODER = ENCODERS[NUM];
+    return ENCODER.getPosition();
+  }
 
   /**
    * Retrieves the existing instance of this static utility class
