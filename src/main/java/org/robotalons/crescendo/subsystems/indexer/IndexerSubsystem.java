393079cdeef1aebb332315c745a740c17985907f
Quinn Jones <66756011+SpiderFace@users.noreply.github.com>
2024-02-22 19:15:49 -0600

    Remove Encoder import
    Not used or needed

--- src/main/java/org/robotalons/crescendo/subsystems/indexer/IndexerSubsystem.java
+++ src/main/java/org/robotalons/crescendo/subsystems/indexer/IndexerSubsystem.java
@@ -5,7 +5,6 @@ package org.robotalons.crescendo.subsystems.indexer;
 import java.io.Closeable;
 
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import com.revrobotics.CANSparkMax;
