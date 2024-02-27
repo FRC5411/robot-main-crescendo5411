66f3ad089bdc7c32e7411c1f4f91495959a42d2e
Abishta C <ultimate.abishta+github@gmail.com>
2024-02-26 19:56:30 -0600

    Created motor as well as setVoltage

--- src/main/java/org/robotalons/crescendo/subsystems/elevator/Constants.java
+++ src/main/java/org/robotalons/crescendo/subsystems/elevator/Constants.java
@@ -1,5 +1,10 @@
 package org.robotalons.crescendo.subsystems.elevator;
 
 public class Constants {
-    
+    public class Ports {
+        public static final Integer ELEVATOR_PORT = (0);
+    }
+    public class Measurements{
+        public static final Integer CURRENT_LIMIT = (60);
+    }
 }
--- src/main/java/org/robotalons/crescendo/subsystems/elevator/ElevatorSubsystem.java
+++ src/main/java/org/robotalons/crescendo/subsystems/elevator/ElevatorSubsystem.java
@@ -3,15 +3,43 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package org.robotalons.crescendo.subsystems.elevator;
+import java.io.Closeable;
+import java.io.IOException;
 
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ElevatorSubsystem extends SubsystemBase {
+public class ElevatorSubsystem extends SubsystemBase implements Closeable{
   /** Creates a new ElevatorSubsystem. */
-  public ElevatorSubsystem() {}
+  private static CANSparkMax elevatorMotor;
+  private static ElevatorSubsystem instance;
+
+  public ElevatorSubsystem() {
+    elevatorMotor = new CANSparkMax(Constants.Ports.ELEVATOR_PORT,MotorType.kBrushless);
+    elevatorMotor.setIdleMode(IdleMode.kBrake);
+    elevatorMotor.restoreFactoryDefaults();
+    elevatorMotor.clearFaults();
+
+    elevatorMotor.setSmartCurrentLimit(Constants.Measurements.CURRENT_LIMIT);
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
+
+  public void setVoltage(int volts){
+    elevatorMotor.set(MathUtil.clamp(volts,-12, 12));
+  }
+
+  public synchronized void close() {
+    elevatorMotor.close();
+}
+
+
 }
