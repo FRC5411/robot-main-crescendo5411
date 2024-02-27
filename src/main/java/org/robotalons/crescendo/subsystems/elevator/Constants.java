76558735676adcd52311f6253e66d76a932390fa
Abishta C <ultimate.abishta+github@gmail.com>
2024-02-26 20:18:08 -0600

    Started on Feedforward

--- src/main/java/org/robotalons/crescendo/subsystems/elevator/Constants.java
+++ src/main/java/org/robotalons/crescendo/subsystems/elevator/Constants.java
@@ -3,8 +3,17 @@ package org.robotalons.crescendo.subsystems.elevator;
 public class Constants {
     public class Ports {
         public static final Integer ELEVATOR_PORT = (0);
+        public class Encoder{
+            public static final Integer CHANNEL_A = (1);
+            public static final Integer CHANNEL_B = (2);
+        }
     }
     public class Measurements{
         public static final Integer CURRENT_LIMIT = (60);
+        public class Feedforward{
+            public static final Double KS = (0.0);
+            public static final Double KG = (0.0);
+            public static final Double KV = (0.0);
+        }
     }
 }
--- src/main/java/org/robotalons/crescendo/subsystems/elevator/ElevatorSubsystem.java
+++ src/main/java/org/robotalons/crescendo/subsystems/elevator/ElevatorSubsystem.java
@@ -8,15 +8,23 @@ import java.io.IOException;
 
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
+import org.littletonrobotics.junction.Logger;
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.units.Measure;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 
 public class ElevatorSubsystem extends SubsystemBase implements Closeable{
   /** Creates a new ElevatorSubsystem. */
   private static CANSparkMax elevatorMotor;
+  private static Encoder encoder;
+  private static ElevatorFeedforward feedforward;
   private static ElevatorSubsystem instance;
 
   public ElevatorSubsystem() {
@@ -24,13 +32,20 @@ public class ElevatorSubsystem extends SubsystemBase implements Closeable{
     elevatorMotor.setIdleMode(IdleMode.kBrake);
     elevatorMotor.restoreFactoryDefaults();
     elevatorMotor.clearFaults();
-
     elevatorMotor.setSmartCurrentLimit(Constants.Measurements.CURRENT_LIMIT);
+
+    encoder = new Encoder(Constants.Ports.Encoder.CHANNEL_A,Constants.Ports.Encoder.CHANNEL_B);
+
+    feedforward = new ElevatorFeedforward(
+      Constants.Measurements.Feedforward.KS,
+      Constants.Measurements.Feedforward.KG, 
+      Constants.Measurements.Feedforward.KV);
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    Logger.recordOutput("Elevator Speed", elevatorMotor.get());
   }
 
   public void setVoltage(int volts){
