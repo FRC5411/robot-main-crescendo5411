f14dcfc37aeca2532d10c2af679d56b4b697d7bc
AryanKammili <140930533+AryanKammili@users.noreply.github.com>
2024-03-12 12:45:06 -0500

    Copied Swerve

--- src/main/java/org/robotalons/crescendo/Constants.java
+++ src/main/java/org/robotalons/crescendo/Constants.java
@@ -1,15 +1,25 @@
-// Copyright (c) 2024 FRC 6328
+// Copyright 2021-2024 FRC 6328
 // http://github.com/Mechanical-Advantage
 //
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file at
-// the root directory of this project.
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// version 3 as published by the Free Software Foundation or
+// available in the root directory of this project.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
 
+<<<<<<< Updated upstream
 package org.robotalons.crescendo;
 
 import edu.wpi.first.wpilibj.RobotBase;
 import org.robotalons.crescendo.util.Alert;
 import org.robotalons.crescendo.util.Alert.AlertType;
+=======
+package frc.robot;
+>>>>>>> Stashed changes
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
@@ -20,27 +30,12 @@ import org.robotalons.crescendo.util.Alert.AlertType;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  public static final double loopPeriodSecs = 0.02;
-  private static RobotType robotType = RobotType.COMPBOT;
-  public static final boolean tuningMode = false;
 
-  public static RobotType getRobot() {
-    if (!disableHAL && RobotBase.isReal() && robotType == RobotType.SIMBOT) {
-      new Alert("Invalid robot selected, using competition robot as default.", AlertType.ERROR)
-          .set(true);
-      robotType = RobotType.COMPBOT;
-    }
-    return robotType;
-  }
+  public static final int k_driverID = 0;
+  public static final int k_operatorID = 1;
+  public static final Mode currentMode = Mode.REAL;
 
-  public static Mode getMode() {
-    return switch (robotType) {
-      case DEVBOT, COMPBOT -> RobotBase.isReal() ? Mode.REAL : Mode.REPLAY;
-      case SIMBOT -> Mode.SIM;
-    };
-  }
-
-  public enum Mode {
+  public static enum Mode {
     /** Running on a real robot. */
     REAL,
 
@@ -49,25 +44,5 @@ public final class Constants {
 
     /** Replaying from a log file. */
     REPLAY
-  }
-
-  public enum RobotType {
-    SIMBOT,
-    DEVBOT,
-    COMPBOT
-  }
-
-  public static boolean disableHAL = false;
-
-  public static void disableHAL() {
-    disableHAL = true;
-  }
-
-  /** Checks whether the robot the correct robot is selected when deploying. */
-  public static void main(String... args) {
-    if (robotType == RobotType.SIMBOT) {
-      System.err.println("Cannot deploy, invalid robot selected: " + robotType);
-      System.exit(1);
-    }
   }
 }
--- src/main/java/org/robotalons/crescendo/subsystems/drive/ModuleLimits.java
+++
@@ -1,5 +0,0 @@
-package org.robotalons.crescendo.subsystems.drive;
-
-public record ModuleLimits(
-    double maxDriveVelocity, double maxDriveAcceleration, double maxSteeringVelocity) {}
-    
