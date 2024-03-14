44645e33072d64c6610e06dfbbefdc1e3262f7d0
Cody Washington <115746975+Jelatinone@users.noreply.github.com>
2024-03-13 23:32:10 -0500

    Add Wait & Notify

--- src/test/java/org/robotalons/crescendo/ExecutionTest.java
+++ src/test/java/org/robotalons/crescendo/ExecutionTest.java
@@ -10,19 +10,32 @@ import java.util.concurrent.TimeUnit;
 public final class ExecutionTest {
   // --------------------------------------------------------------[Constants]--------------------------------------------------------------//
   public static final Long TEST_RUNTIME_DURATION = (5_000L);
-  public static final Long TEST_START_DURATION = (5_000L);
+  public static final Long TEST_START_DURATION = (7_500L);
   // ---------------------------------------------------------------[Methods]---------------------------------------------------------------//
   @Test
   synchronized void checkErrorRuntime() throws Exception {
     final var RuntimeExecutor = Executors.newFixedThreadPool((1));
     RuntimeExecutor.submit(() -> Main.main());
     TimeUnit.MILLISECONDS.sleep(TEST_START_DURATION);
+    RuntimeExecutor.execute(() -> {
+      try {
+        RuntimeExecutor.wait();
+      } catch(final Exception Ignored) {
+        Ignored.printStackTrace();
+      }
+    });
     final var RuntimeInputs = LoggedDriverStation.class.getDeclaredField(("dsInputs"));
     RuntimeInputs.setAccessible((true));
-    final var RuntimeValue = RuntimeInputs.get((null));
-    final var RuntimeEnabled = RuntimeValue.getClass().getDeclaredField(("enabled"));
+    final var RuntimeEnabled = LoggedDriverStation.getDSData().getClass().getDeclaredField(("enabled"));
     RuntimeEnabled.setAccessible((true));
-    RuntimeEnabled.setBoolean(RuntimeValue, (true));
+    RuntimeEnabled.setBoolean(LoggedDriverStation.getDSData(), (true));
+    RuntimeExecutor.execute(() -> {
+      try {
+        RuntimeExecutor.notify();
+      } catch(final Exception Ignored) {
+        Ignored.printStackTrace();
+      }
+    });
     TimeUnit.MILLISECONDS.sleep(TEST_RUNTIME_DURATION);
     RuntimeExecutor.shutdownNow();
   }
