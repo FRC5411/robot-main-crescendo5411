ff4869d5866fec2eac39b66f5d11d4f65ea58fb1
Cody Washington <115746975+Jelatinone@users.noreply.github.com>
2024-03-13 23:23:12 -0500

    Working Test

--- src/test/java/org/robotalons/crescendo/ExecutionTest.java
+++ src/test/java/org/robotalons/crescendo/ExecutionTest.java
@@ -3,32 +3,27 @@ package org.robotalons.crescendo;
 // ---------------------------------------------------------------[Libraries]---------------------------------------------------------------//
 import org.junit.jupiter.api.Test;
 import org.littletonrobotics.junction.inputs.LoggedDriverStation;
-import org.littletonrobotics.junction.inputs.LoggedDriverStation.DriverStationInputs;
 
-import java.util.concurrent.CancellationException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 // -------------------------------------------------------------[Execution Test]-----------------------------------------------------------//
 public final class ExecutionTest {
   // --------------------------------------------------------------[Constants]--------------------------------------------------------------//
-  public static final Long TEST_DURATION = (5000L);
+  public static final Long TEST_RUNTIME_DURATION = (5_000L);
+  public static final Long TEST_START_DURATION = (5_000L);
   // ---------------------------------------------------------------[Methods]---------------------------------------------------------------//
-  /**
-   * Instantiates a new robot, asserts that no errors are thrown within the alloted time limit.
-   */
   @Test
-  synchronized void checkErrorMain() throws Exception {
-    final var Executor = Executors.newFixedThreadPool((1));
-    Executor.submit(() -> Main.main());
-    TimeUnit.MILLISECONDS.sleep(TEST_DURATION);
-    final var Field = LoggedDriverStation.class.getField(("dsInputs"));
-    final var Faked = new DriverStationInputs();
-    Executor.wait();
-    Faked.enabled = (true);
-    Field.setAccessible((true));
-    Field.set((null), Faked);
-    Executor.notifyAll();
-    TimeUnit.MILLISECONDS.sleep(TEST_DURATION);
-    Executor.shutdownNow();    
+  synchronized void checkErrorRuntime() throws Exception {
+    final var RuntimeExecutor = Executors.newFixedThreadPool((1));
+    RuntimeExecutor.submit(() -> Main.main());
+    TimeUnit.MILLISECONDS.sleep(TEST_START_DURATION);
+    final var RuntimeInputs = LoggedDriverStation.class.getDeclaredField(("dsInputs"));
+    RuntimeInputs.setAccessible((true));
+    final var RuntimeValue = RuntimeInputs.get((null));
+    final var RuntimeEnabled = RuntimeValue.getClass().getDeclaredField(("enabled"));
+    RuntimeEnabled.setAccessible((true));
+    RuntimeEnabled.setBoolean(RuntimeValue, (true));
+    TimeUnit.MILLISECONDS.sleep(TEST_RUNTIME_DURATION);
+    RuntimeExecutor.shutdownNow();
   }
 }
